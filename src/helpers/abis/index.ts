cc58ba6b7d39905762be359246593bb59b173368
YutaSugimura <nextc3m@gmail.com>
2022-01-13 17:46:21 +0900

    add: helpers/abis/erc1155

--- src/helpers/abis/index.ts
+++ src/helpers/abis/index.ts
@@ -1,2 +1,3 @@
 export { ERC20_ABI } from './erc20';
 export { ERC721_ABI } from './erc721';
+export { ERC1155_ABI } from './erc1155';
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,3 @@
 export { txType1, txType2 } from './transaction';
-export { ERC20_ABI, ERC721_ABI } from './abis';
+export { ERC20_ABI, ERC721_ABI, ERC1155_ABI } from './abis';
 export { numberOfTokens, formatGasPrice } from './utils/unit';
