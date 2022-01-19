8f0699aa796c24d307deff184ee7e439594e003c
YutaSugimura <nextc3m@gmail.com>
2022-01-20 00:22:01 +0900

    update: ./test/helper.test.ts

--- test/helper.test.ts
+++ test/helper.test.ts
@@ -1,13 +1,21 @@
 import dotEnv from 'dotenv';
-import { numberOfTokens, formatGasPrice, type Provider, getProvider } from '../src';
+import {
+  type Provider,
+  numberOfTokens,
+  formatGasPrice,
+  getProvider,
+  getEnsResolver,
+  lookupAddress,
+  resolveName,
+} from '../src';
 
-const provider: Provider | null = null;
+let provider: Provider | null = null;
 
-// beforeAll(() => {
-//   dotEnv.config();
+beforeAll(() => {
+  dotEnv.config();
 
-//   provider = getProvider(process.env.JSON_RPC_URL) as Provider;
-// });
+  provider = getProvider(process.env.MAIN_NET_JSON_RPC_URL) as Provider;
+});
 
 describe('Test helper', () => {
   it('Test the numberOfTokens', () => {
@@ -20,4 +28,30 @@ describe('Test helper', () => {
     const bigNumber = formatGasPrice(12);
     expect(bigNumber.toHexString()).toBe('0x02cb417800');
   });
+
+  it('Test the lookupAddress', async () => {
+    const ensName = await lookupAddress(provider, '0x983110309620D911731Ac0932219af06091b6744');
+    expect(ensName).toBe('brantly.eth');
+  }, 15000);
+
+  it('Test the lookupAddress(null)', async () => {
+    const ensName = await lookupAddress(provider, '0xb6E040C9ECAaE172a89bD561c5F73e1C48d28cd9');
+    expect(ensName).toBe(null);
+  }, 15000);
+
+  it('Test the resolveName', async () => {
+    const result = await resolveName(provider, 'brantly.eth');
+    expect(result).toBe('0x983110309620D911731Ac0932219af06091b6744');
+  }, 15000);
+
+  it('Test the resolveName', async () => {
+    const result = await resolveName(provider, 'ens.eth');
+    expect(result).toBe('0xb6E040C9ECAaE172a89bD561c5F73e1C48d28cd9');
+  }, 15000);
+
+  it('Test the getEnsResolver', async () => {
+    const ens = await getEnsResolver(provider, 'ens.eth');
+    const result = await ens.getAvatar();
+    expect(result.url).toBeDefined();
+  }, 15000);
 });
